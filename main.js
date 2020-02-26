1811429588998f54653319b8e3eef1cef07515ce
Harry <harrychy8@gmail.com>
2020-02-25 21:51:24 -0500

    (-) Minor bug fixes + main.js clean up

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const electron = require('electron');
-const { dialog } = require('electron')
+const {dialog} = require('electron');
 const url = require('url');
 const path = require('path');
 const R = require("r-script");
 const spawn = require('child_process').spawn;
-const PDFWindow = require('electron-pdf-window')
+const PDFWindow = require('electron-pdf-window');
 
 const {app, BrowserWindow, Menu, ipcMain} = electron;
 
@@ -41,199 +41,194 @@ app.on('ready', function () {
 
 //Catch r:step1
 ipcMain.on('r:step1', function (e, input) {
-    test1();
     e.reply('r:step1:reply');
 });
 
-function test1() {
-    for (let i = 0; i < 50; i++) setup_R_job("!!!");
-}
-
 //Catch Barcode selection
 ipcMain.on('barcodeSelection', function (e, name, snap, csv) {
-    child = runR('./Rscripts/barcodeSelection.R', ["./data/" + snap, "./data/" + csv, name]);
-    child.on('exit', function (code) {
+    let child = runR('./Rscripts/barcodeSelection.R', ["./data/" + snap, "./data/" + csv, name]);
+    child.on('exit', function () {
         e.reply('barcodeSelection:reply');
     });
 });
 
 ipcMain.on('primary', function (e, name, snap, csv, blacklist) {
-    child = runR('./Rscripts/primary.R', ["./data/" + snap, "./data/" + csv, name, "./data/" + blacklist], e);
+    let child = runR('./Rscripts/primary.R', ["./data/" + snap, "./data/" + csv, name, "./data/" + blacklist], e);
     child.on('exit', function (code) {
         e.reply('primary:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('dimReduction', function (e, snap) {
-    child = runR('./Rscripts/dimReduction.R', ["./data/" + snap]);
+    let child = runR('./Rscripts/dimReduction.R', ["./data/" + snap]);
     child.on('exit', function (code) {
         e.reply('dimReduction:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('plotDimReductPW', function (e, name, snap) {
-    child = runR('./Rscripts/plotDimReductPW.R', [name, "./data/" + snap]);
+    let child = runR('./Rscripts/plotDimReductPW.R', [name, "./data/" + snap]);
     child.on('exit', function (code) {
         e.reply('plotDimReductPW:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('GBclustering', function (e, snap) {
-    child = runR('./Rscripts/GBclustering.R', ["./data/" + snap]);
+    let child = runR('./Rscripts/GBclustering.R', ["./data/" + snap]);
     child.on('exit', function (code) {
         e.reply('GBclustering:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('visualization', function (e, name, snap) {
-    child = runR('./Rscripts/visualization.R', [name, "./data/" + snap]);
+    let child = runR('./Rscripts/visualization.R', [name, "./data/" + snap]);
     child.on('exit', function (code) {
         e.reply('visualization:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('geneBasedAnnotation', function (e, name, snap, gene) {
-    child = runR('./Rscripts/geneBasedAnnotation.R', [name, "./data/" + snap, gene]);
+    let child = runR('./Rscripts/geneBasedAnnotation.R', [name, "./data/" + snap, gene]);
     child.on('exit', function (code) {
         e.reply('geneBasedAnnotation:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('runViz', function (e, snap, method) {
-    child = runR('./Rscripts/runViz.R', ["./data/" + snap, method]);
+    let child = runR('./Rscripts/runViz.R', ["./data/" + snap, method]);
     child.on('exit', function (code) {
         e.reply('runViz:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('plotViz', function (e, name, snap, method) {
-    child = runR('./Rscripts/plotViz.R', ["./data/" + snap, name, method]);
+    let child = runR('./Rscripts/plotViz.R', ["./data/" + snap, name, method]);
     child.on('exit', function (code) {
         e.reply('plotViz:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('plotFeatureSingle', function (e, name, snap, method) {
-    child = runR('./Rscripts/plotFeatureSingle.R', ["./data/" + snap, name, method]);
+    let child = runR('./Rscripts/plotFeatureSingle.R', ["./data/" + snap, name, method]);
     child.on('exit', function (code) {
         e.reply('plotFeatureSingle:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('geneBasedAnnotation', function (e, snap, table) {
-    child = runR('./Rscripts/geneBasedAnnotation.R', ["./data/" + snap, "./data/" + table]);
+    let child = runR('./Rscripts/geneBasedAnnotation.R', ["./data/" + snap, "./data/" + table]);
     child.on('exit', function (code) {
         e.reply('geneBasedAnnotation:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('rnaBasedAnnotation', function (e, snap, table) {
-    child = runR('./Rscripts/rnaBasedAnnotation.R', ["./data/" + snap, "./data/" + table]);
+    let child = runR('./Rscripts/rnaBasedAnnotation.R', ["./data/" + snap, "./data/" + table]);
     child.on('exit', function (code) {
         e.reply('rnaBasedAnnotation:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
 ipcMain.on('hereticalClustering', function (e, name, snap) {
-    child = runR('./Rscripts/hereticalClustering.R', ["./data/" + snap, name]);
+    let child = runR('./Rscripts/hereticalClustering.R', ["./data/" + snap, name]);
     child.on('exit', function (code) {
         e.reply('hereticalClustering:reply');
-        message = code ? 'Failure' : 'Success';
+        let message = code ? 'Failure' : 'Success';
         dialog.showMessageBoxSync(mainWindow, {
             type: 'info',
             buttons: [],
-            title:'Result',
+            title: 'Result',
             message: 'Process has been completed',
             detail: message,
-          })
+        })
     });
 });
 
@@ -246,49 +241,53 @@ function runR(script, params, event) {
     const R = spawn('Rscript', RCall);
 
     const step_name = RCall[0].substring(0, RCall[0].indexOf('.'));
-    console.log("Step:"+step_name);
+    console.log("Step:" + step_name);
 
-    var scriptOutput = "";
+    let scriptOutput = "";
 
     R.stdout.setEncoding('utf8');
-    R.stdout.on('data', function(data) {
+    R.stdout.on('data', function (data) {
         console.log('stdout: ' + data);
 
-        data=data.toString();
-        scriptOutput+=data;
+        data = data.toString();
+        scriptOutput += data;
         event.reply("console:log", data);
     });
 
     R.stderr.setEncoding('utf8');
-    R.stderr.on('data', function(data) {
+    R.stderr.on('data', function (data) {
         console.log('stderr: ' + data);
 
-        data=data.toString();
-        scriptOutput+=data;
+        data = data.toString();
+        scriptOutput += data;
         event.reply("console:log", data);
     });
 
     console.log(scriptOutput);
 
-    R.on('exit',function(code){
-        console.log('got exit code: '+code);
-        if(code===1){
+    R.on('exit', function (code) {
+        let filename;
+        console.log('got exit code: ' + code);
+        let plotWindow;
+        let plotWindow2;
+        let plotWindow1;
+        if (code === 1) {
             // do something special
             console.log("failure")
-        }else{
-            console.log("success")
-            
-            const output_path = __dirname+"/output";
-            if(step_name === 'primary'){
+        } else {
+            console.log("success");
 
-                const barcode = "/"+RCall[3]+".barcodeSelection.png";
-                const histogram = "/"+RCall[3]+".histogram.png";
+            const output_path = __dirname + "/output";
+            if (step_name === 'primary') {
+
+                const barcode = "/" + RCall[3] + ".barcodeSelection.png";
+                const histogram = "/" + RCall[3] + ".histogram.png";
 
                 console.log("Loading plots...");
                 plotWindow1 = new BrowserWindow({
                     useContentSize: true
                 });
-                
+
                 plotWindow1.loadURL(url.format({
                     pathname: path.join(output_path, barcode),
                     protocol: 'file:',
@@ -298,51 +297,48 @@ function runR(script, params, event) {
                 plotWindow2 = new BrowserWindow({
                     useContentSize: true
                 });
-                
+
                 plotWindow2.loadURL(url.format({
                     pathname: path.join(output_path, histogram),
                     protocol: 'file:',
                     slashes: true
                 }));
-                
-            } 
-            else {
 
-                if (step_name === 'plotDimReductPW' || step_name === 'plotViz' || step_name === 'plotFeatureSingle'){
+            } else {
+
+                if (step_name === 'plotDimReductPW' || step_name === 'plotViz' || step_name === 'plotFeatureSingle') {
 
                     console.log("Loading plot...");
 
-                    var filename = "."+step_name+".pdf";
-                    
+                    filename = "." + step_name + ".pdf";
+
                     if (step_name === 'plotDimReductPW') {
-                        filename = "/"+RCall[1]+filename;
-                    }
-                    else {
-                        filename = "/"+RCall[2]+filename;
+                        filename = "/" + RCall[1] + filename;
+                    } else {
+                        filename = "/" + RCall[2] + filename;
                     }
 
                     plotWindow = new PDFWindow({
                         useContentSize: true
                     });
-                    
+
                     plotWindow.loadURL(url.format({
                         pathname: path.join(output_path, filename),
                         protocol: 'file:',
                         slashes: true
                     }));
-                    
-                }
-                else {
-                    if (RCall[3] === undefined){
+
+                } else {
+                    if (RCall[3] === undefined) {
                         // do nothing;
                     } else {
-                        var filename = "/"+RCall[3]+"."+step_name;
-                        filename = filename+".png";
+                        filename = "/" + RCall[3] + "." + step_name;
+                        filename = filename + ".png";
 
                         plotWindow = new BrowserWindow({
                             useContentSize: true
                         });
-                        
+
                         plotWindow.loadURL(url.format({
                             pathname: path.join(output_path, filename),
                             protocol: 'file:',
@@ -350,7 +346,7 @@ function runR(script, params, event) {
                         }));
                     }
                 }
-                
+
             }
         }
         return null;
